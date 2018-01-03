17257d11f9f10ef9cabc42a83808a6895278cb3a
Adão Braga <braganet@live.com>
2018-01-03 19:27:34 -0200

    update files

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require_once ('config.php');
 
-$sql      = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+#$sql      = new Sql();
+#$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+$user = new Usuario();
+$user->loadById(1);
+
+echo $user;
