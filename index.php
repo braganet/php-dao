4b3c1f6352ed9150c831c272a77c8190926c6a24
Adão Braga <braganet@live.com>
2018-01-04 15:33:55 -0200

    enviando atualizacoes

--- index.php
+++ index.php
@@ -5,7 +5,18 @@ require_once ('config.php');
 #$sql      = new Sql();
 #$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-$user = new Usuario();
-$user->loadById(1);
+//Carrega 1 usuário
+#$user = new Usuario();
+#$user->loadById(4);
 
-echo $user;
+//Carrega lista de usuários
+#$lista = Usuario::getList();
+
+//Carrega lista de usuários
+#$search = Usuario::search("jo");
+
+//Carrega um usuários usando um login e senha
+$usuario = new Usuario();
+$usuario->login("jose", "123456789");
+
+echo $usuario;
